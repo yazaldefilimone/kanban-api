c4c895f5fe17568b4ea038106ff9aea98fe9bb1f
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-18 23:25:36 +0200

    fix: add omit password

--- src/application/repositories/user/user-repository.ts
+++ src/application/repositories/user/user-repository.ts
@@ -2,7 +2,7 @@ import { userStoreType } from '~/domain/user/dtos';
 
 export interface IUserRepository {
   sign: (data: userStoreType) => Promise<{ id: string }>;
-  getId: ({ id }: { id: string }) => Promise<userStoreType | null>;
+  getId: ({ id }: { id: string }) => Promise<Omit<userStoreType, 'password'> | null>;
   getEmail: ({ email }: { email: string }) => Promise<userStoreType | null>;
-  getAll: () => Promise<userStoreType[]>;
+  getAll: () => Promise<Omit<userStoreType, 'password'>[]>;
 }
