162d7f0ada9ff2c8d4d3542659ada270fbed92db
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-18 23:54:29 +0200

    feat: create factory of search user

---
+++ src/main/factories/user/search-user-factory.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { SearchUserUseCase } from '~/application/use-cases/user';
+import { SearchUserController } from '~/infrastructure/controllers/user';
+import { UserRepository } from '~/infrastructure/repositories/user';
+
+export const searchUserFactory = async function (request: Request, response: Response) {
+  const userRepository = new UserRepository();
+  const searchUserUseCase = new SearchUserUseCase(userRepository);
+  const searchUserController = new SearchUserController(searchUserUseCase);
+  return searchUserController.execute({ request, response });
+};
--- src/main/routes/user/index.ts
+++ src/main/routes/user/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { makeSignUserFactory } from '~/main/factories/user';
+import { signUserFactory } from '~/main/factories/user';
+import { searchUserFactory } from '~/main/factories/user/search-user-factory';
 
 const userRoutes = Router();
 
-userRoutes.post('/sign', makeSignUserFactory);
+userRoutes.post('/sign', signUserFactory);
+userRoutes.get('/search', searchUserFactory);
 
 export { userRoutes };
