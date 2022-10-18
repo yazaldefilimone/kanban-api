c3e92110e2df51c70f716313ee9762dc3e9ff734
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-18 23:50:27 +0200

    feat: implement search user

--- src/application/use-cases/user/index.ts
+++ src/application/use-cases/user/index.ts
@@ -1,1 +1,2 @@
 export * from './sign-user-usecase';
+export * from './search-user-usecase';
---
+++ src/application/use-cases/user/search-user-usecase.ts
@@ -0,0 +1,37 @@
+import { IUserRepository } from '~/application/repositories/user';
+import { userStoreType } from '~/domain/user/dtos';
+import { User } from '~/domain/user/entity';
+import { ISearchUserUseCase } from '~/domain/user/use-cases';
+import { left, right } from '~/shared/either';
+
+export class SearchUserUseCase implements ISearchUserUseCase {
+  private readonly userRepository: IUserRepository;
+  constructor(userRepository: IUserRepository) {
+    this.userRepository = userRepository;
+  }
+
+  async perform(input: ISearchUserUseCase.Input): ISearchUserUseCase.Output {
+    let user: Omit<userStoreType, 'password'> | Omit<userStoreType, 'password'>[];
+    const { email, id } = input;
+
+    if (email) {
+      const result = User.isValidEmail(email);
+
+      if (result.isLeft()) {
+        return left(result.value);
+      }
+      const userEmail = await this.userRepository.getEmail({ email: result.value });
+      delete userEmail.password;
+      return right(userEmail);
+    }
+
+    if (id) {
+      user = await this.userRepository.getId({ id });
+      return right(user);
+    }
+
+    const users = await this.userRepository.getAll();
+
+    return right(users);
+  }
+}
