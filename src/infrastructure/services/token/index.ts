3d8f60390effd2de4b28af8f5f5c01f596ff328e
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-18 20:33:21 +0200

    feat: create implementation of token

---
+++ src/infrastructure/services/token/index.ts
@@ -0,0 +1,34 @@
+import { ITokenGenerator, ITokenValidator } from '~/application/services/token';
+import { JwtPayload, sign, verify } from 'jsonwebtoken';
+import { env } from '~/shared/env';
+type ResponseToken = {
+  status: boolean;
+  data: string | JwtPayload;
+};
+export class TokenGenerator implements ITokenGenerator {
+  async generate({ key, expirationInMs }: ITokenGenerator.Input): Promise<ITokenGenerator.Output> {
+    const expiration = expirationInMs ? expirationInMs : '1d';
+    const token = sign(key, env.token.privateKey, { expiresIn: expiration });
+    return token;
+  }
+}
+
+export class TokenValidator {
+  async validate({ token }: ITokenValidator.Input): Promise<ITokenValidator.Output<ResponseToken>> {
+    let response: ITokenValidator.Output<ResponseToken>;
+    verify(token, env.token.privateKey, (error, decoder) => {
+      if (error) {
+        response = {
+          status: false,
+          data: error.message as any,
+        };
+      }
+      response = {
+        status: true,
+        data: decoder as any,
+      };
+    });
+
+    return response;
+  }
+}
