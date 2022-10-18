25a1ba48aebf2dc24e6ce1174c982a1dcac03b01
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-18 20:35:29 +0200

    feat: add other dependece of feactor sign user

--- src/main/factories/user/make-sign-user-factory.ts
+++ src/main/factories/user/make-sign-user-factory.ts
@@ -2,10 +2,14 @@ import { Request, Response } from 'express';
 import { SignUserUseCase } from '~/application/use-cases/user';
 import { SignUserController } from '~/infrastructure/controllers/user';
 import { UserRepository } from '~/infrastructure/repositories/user';
+import { Cryptography } from '~/infrastructure/services/cryptography';
+import { TokenGenerator } from '~/infrastructure/services/token';
 
 export const makeSignUserFactory = async function (request: Request, response: Response) {
   const userRepository = new UserRepository();
-  const signUserUseCase = new SignUserUseCase(userRepository);
+  const tokenGenerator = new TokenGenerator();
+  const cryptography = new Cryptography();
+  const signUserUseCase = new SignUserUseCase(userRepository, tokenGenerator, cryptography);
   const signUserController = new SignUserController(signUserUseCase);
   return signUserController.execute({ request, response });
 };
