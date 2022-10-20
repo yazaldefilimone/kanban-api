9f386d7f63fe8e284488cb393bf1de15b6075941
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-20 22:29:15 +0200

    feat: create get name repository

--- src/application/repositories/user/user-repository.ts
+++ src/application/repositories/user/user-repository.ts
@@ -4,5 +4,6 @@ export interface IUserRepository {
   sign: (data: userStoreType) => Promise<{ id: string }>;
   getId: ({ id }: { id: string }) => Promise<Omit<userStoreType, 'password'> | null>;
   getEmail: ({ email }: { email: string }) => Promise<userStoreType | null>;
+  getName: ({ name }: { name: string }) => Promise<Omit<userStoreType, 'password'>[] | null>;
   getAll: () => Promise<Omit<userStoreType, 'password'>[]>;
 }
