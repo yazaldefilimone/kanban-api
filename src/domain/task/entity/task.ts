a314b59059ec39b287b96ddc15158a01c5e9e458
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-22 23:20:23 +0200

    feat: create task entity

---
+++ src/domain/task/entity/task.ts
@@ -0,0 +1,49 @@
+import { randomUUID } from 'node:crypto';
+import { InvalidParamError } from '~/domain/errors';
+import { taskStoreType, taskType } from '~/domain/task/dtos';
+import { Either, left, right } from '~/shared/either';
+import { isValidDescription, isValidName } from '~/shared/validators';
+import { taskBuildResponse } from './ports';
+
+export class Task {
+  public task: taskStoreType;
+  private constructor(task: taskType) {
+    this.task = {
+      id: randomUUID(),
+      ...task,
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+    };
+  }
+
+  public static isValidDescription(description: string): Either<InvalidParamError, string> {
+    return isValidDescription(description) ? right(description) : left(new InvalidParamError({ param: 'description' }));
+  }
+
+  public static isValidName(name: string): Either<InvalidParamError, string> {
+    return isValidName(name) ? right(name) : left(new InvalidParamError({ param: 'name' }));
+  }
+
+  public static build(task: taskType): taskBuildResponse {
+    const taskValidate = {
+      description: this.isValidDescription(task.description),
+      name: this.isValidName(task.name),
+    };
+
+    if (taskValidate.description.isLeft()) {
+      return left(taskValidate.description.value);
+    }
+    if (taskValidate.name.isLeft()) {
+      return left(taskValidate.name.value);
+    }
+    const taskValid: taskType = {
+      description: taskValidate.description.value,
+      name: taskValidate.name.value,
+      statusId: task.statusId,
+    };
+
+    const metadata = new Task(taskValid);
+
+    return right(metadata.task);
+  }
+}
