945555f64859e30aa9a8a6ffa5052fdc37efe98c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-22 21:56:37 +0200

    fix: add optional next function

--- src/infrastructure/controllers/protocols/index.ts
+++ src/infrastructure/controllers/protocols/index.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 export interface IController {
   execute: (Input: IController.Input) => Promise<IController.Output>;
 }
 
 export namespace IController {
-  export type Input = { request: Request; response: Response };
+  export type Input = { request: Request; response: Response; next?: NextFunction };
   export type Output = Response;
 }
