bc16be6f9b3a8b5ff2ae2b9f86970de4d5d5953f
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-22 21:55:47 +0200

    feat: create middlewares auth user

---
+++ src/infrastructure/controllers/middlewares/auth-middleware.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from 'express';
+import { ITokenValidator } from '~/application/services/token';
+
+export class AuthMiddleware {
+  private readonly tokenValidator: ITokenValidator;
+  constructor(tokenValidator: ITokenValidator) {
+    this.tokenValidator = tokenValidator;
+  }
+
+  async execute(request: Request, response: Response, next: NextFunction): Promise<any> {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      return response.status(401).json({ error: 'No token provided' });
+    }
+
+    const parts = authHeader.split(' ');
+
+    if (!(parts.length === 2)) {
+      return response.status(401).json({ error: 'Token properties error' });
+    }
+
+    const [scheme, token] = parts;
+
+    if (!/^Bearer$/.test(scheme)) {
+      return response.status(401).json({ error: 'Token invalid' });
+    }
+
+    const validation = await this.tokenValidator.validate({ token });
+
+    if (!validation.status) {
+      return response.status(401).json({ error: 'Token invalid' });
+    }
+
+    request.userId = validation.data.id as string;
+
+    return next();
+  }
+}
