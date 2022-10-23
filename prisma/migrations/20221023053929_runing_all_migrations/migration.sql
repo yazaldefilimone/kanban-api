1f9b9d0c6c22d19f93730c02a9e46214c94b7a2c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-23 08:08:35 +0200

    feat: run migration

--- prisma/migrations/20221023053929_runing_all_migrations/migration.sql
+++ prisma/migrations/20221023053929_runing_all_migrations/migration.sql
@@ -1,7 +1,22 @@
 -- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "bio" TEXT,
+    "avatar_url" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updateAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "tasks" (
     "id" TEXT NOT NULL,
     "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(500) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL,
     "updateAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" TEXT NOT NULL,
@@ -9,6 +24,16 @@ CREATE TABLE "tasks" (
     "statusId" TEXT NOT NULL,
 
     CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subtasks" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT false,
+    "taskId" TEXT NOT NULL,
+
+    CONSTRAINT "subtasks_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -33,6 +58,9 @@ CREATE TABLE "status" (
     CONSTRAINT "status_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
 -- AddForeignKey
 ALTER TABLE "tasks" ADD CONSTRAINT "tasks_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "status"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -41,6 +69,9 @@ ALTER TABLE "tasks" ADD CONSTRAINT "tasks_userId_fkey" FOREIGN KEY ("userId") RE
 
 -- AddForeignKey
 ALTER TABLE "tasks" ADD CONSTRAINT "tasks_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "boards"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subtasks" ADD CONSTRAINT "subtasks_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "boards" ADD CONSTRAINT "boards_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
