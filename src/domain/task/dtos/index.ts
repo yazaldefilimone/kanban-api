369f207d18f5efdac5792dfc54215f2e21100c8e
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-23 08:10:53 +0200

    feat: add id relation in dtos

--- src/domain/task/dtos/index.ts
+++ src/domain/task/dtos/index.ts
@@ -3,17 +3,23 @@ type Subtasks = {
   name: string;
   status: string;
   taskId: string;
+  userId: string;
+  boardId: string;
 };
 
 export type taskType = {
   name: string;
   description: string;
   statusId: string;
+  userId: string;
+  boardId: string;
 };
 
 export type taskSaveType = {
   name: string;
   description: string;
+  userId: string;
+  boardId: string;
   statusId: string;
 };
 
@@ -22,6 +28,8 @@ export type taskStoreType = {
   name: string;
   description: string;
   statusId: string;
+  userId: string;
+  boardId: string;
   subtasks?: Subtasks[];
   createdAt: string | Date;
   updateAt: string | Date;
