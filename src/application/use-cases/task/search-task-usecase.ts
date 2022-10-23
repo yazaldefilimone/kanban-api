21343d4e536ece0e1605c1a6c2361344fd931350
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-23 08:50:13 +0200

    feat: create  search task usecase

---
+++ src/application/use-cases/task/search-task-usecase.ts
@@ -0,0 +1,22 @@
+import { ITaskRepository } from '~/application/repositories/task';
+import { InvalidParamError } from '~/domain/errors';
+import { ISearchTaskUseCase } from '~/domain/task/use-cases';
+import { left, right } from '~/shared/either';
+
+export class SearchTaskUseCase implements ISearchTaskUseCase {
+  private readonly taskRepository: ITaskRepository;
+  constructor(taskRepository: ITaskRepository) {
+    this.taskRepository = taskRepository;
+  }
+
+  async perform(input: ISearchTaskUseCase.Input): ISearchTaskUseCase.Output {
+    const { statusId } = input;
+
+    if (!statusId) {
+      return left(new InvalidParamError({ param: 'statusId' }));
+    }
+
+    const tasks = await this.taskRepository.getStatus({ statusId });
+    return right(tasks);
+  }
+}
