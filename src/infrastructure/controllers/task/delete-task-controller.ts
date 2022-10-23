df8bde47920a619eaffc8a97e9cd4bb6799b3831
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-23 08:51:37 +0200

    feat: create  delete task controller

---
+++ src/infrastructure/controllers/task/delete-task-controller.ts
@@ -0,0 +1,22 @@
+import { IDeleteTaskUseCase } from '~/domain/task/use-cases';
+import { IController } from '~/infrastructure/controllers/protocols';
+
+export class DeleteTaskController implements IController {
+  private readonly deleteTaskUseCase: IDeleteTaskUseCase;
+  constructor(deleteTaskUseCase: IDeleteTaskUseCase) {
+    this.deleteTaskUseCase = deleteTaskUseCase;
+  }
+  async execute({ request, response }: IController.Input): Promise<IController.Output> {
+    const payload = request.params;
+    if (JSON.stringify(payload) === '{}' || !payload) {
+      return response.status(204).json({ message: 'params is empty' });
+    }
+
+    const taskOrError = await this.deleteTaskUseCase.perform(payload as any);
+    if (taskOrError.isLeft()) {
+      return response.status(400).json({ message: taskOrError.value.message });
+    }
+
+    return response.status(200).json({ message: 'task delete successfully' });
+  }
+}
