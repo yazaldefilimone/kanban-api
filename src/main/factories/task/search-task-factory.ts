5a11ae00b6c15a84ba9f2ae12f3872fea94582cf
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-23 08:58:31 +0200

    feat: create task controller

---
+++ src/main/factories/task/search-task-factory.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { SearchTaskUseCase } from '~/application/use-cases/task';
+import { SearchTaskController } from '~/infrastructure/controllers/task';
+import { TaskRepository } from '~/infrastructure/repositories/task/task-repository';
+
+export const searchTaskFactory = async (request: Request, response: Response) => {
+  const taskRepository = new TaskRepository();
+  const searchTaskUseCase = new SearchTaskUseCase(taskRepository);
+  const searchTaskController = new SearchTaskController(searchTaskUseCase);
+  return await searchTaskController.execute({ request, response });
+};
