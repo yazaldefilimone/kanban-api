67b17177f61b3b7b2f26992e2b6aae4bf09f0add
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-23 08:10:28 +0200

    fix: change types in task repository

--- src/application/repositories/task/task-repository.ts
+++ src/application/repositories/task/task-repository.ts
@@ -3,7 +3,7 @@ import { taskSaveType, taskStoreType } from '~/domain/task/dtos';
 export interface ITaskRepository {
   save: (data: taskSaveType) => Promise<{ id: string }>;
   getId: ({ id }: { id: string }) => Promise<taskStoreType | null>;
-  getStatus: ({ email }: { email: string }) => Promise<taskStoreType[] | null>;
-  getName: ({ name }: { name: string }) => Promise<taskStoreType | null>;
+  getStatus: ({ statusId }: { statusId: string }) => Promise<taskStoreType[] | null>;
+  getName: ({ name }: { name: string }) => Promise<{ id: string } | null>;
   getAll: () => Promise<taskStoreType[]>;
 }
