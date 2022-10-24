c233673d109b8977e511644ef9e8111fb1015a37
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-24 22:07:44 +0200

    fix: change param

--- src/application/repositories/board/board-repository.ts
+++ src/application/repositories/board/board-repository.ts
@@ -3,7 +3,7 @@ import { boardSaveType, boardStoreType } from '~/domain/board/dtos';
 export interface IBoardRepository {
   save: (data: boardSaveType) => Promise<{ id: string }>;
   getId: ({ id }: { id: string }) => Promise<boardStoreType | null>;
-  getUserId: ({ statusId }: { statusId: string }) => Promise<boardStoreType | null>;
+  getUserId: ({ userId }: { userId: string }) => Promise<boardStoreType[] | null>;
   getName: ({ name }: { name: string }) => Promise<{ id: string } | null>;
   delete: ({ id }: { id: string }) => Promise<void>;
   getAll: () => Promise<boardStoreType[]>;
