8b69cda4e52062649cf7dcd093e63c5baa95e92c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-25 12:17:27 +0200

    feat: create factory of board

---
+++ src/main/factories/board/create-board-factory.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { CreateBoardUseCase } from '~/application/use-cases/board';
+import { CreateBoardController } from '~/infrastructure/controllers/board';
+import { BoardRepository } from '~/infrastructure/repositories/board/board-repository';
+
+export const createBoardFactory = async (request: Request, response: Response) => {
+  const boardRepository = new BoardRepository();
+  const createBoardUseCase = new CreateBoardUseCase(boardRepository);
+  const createBoardController = new CreateBoardController(createBoardUseCase);
+  return await createBoardController.execute({ request, response });
+};
