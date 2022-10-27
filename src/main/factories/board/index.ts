d1d8f114a426b87143635dd1306df506ef795210
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-27 16:35:03 +0200

    feat: create search fectory

--- src/main/factories/board/index.ts
+++ src/main/factories/board/index.ts
@@ -1,1 +1,2 @@
 export * from './create-board-factory';
+export * from './search-board-factory';
---
+++ src/main/factories/board/search-board-factory.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { SearchBoardUseCase } from '~/application/use-cases/board';
+import { SearchBoardController } from '~/infrastructure/controllers/board';
+import { BoardRepository } from '~/infrastructure/repositories/board/board-repository';
+
+export const searchBoardFactory = async (request: Request, response: Response) => {
+  const boardRepository = new BoardRepository();
+  const searchBoardUseCase = new SearchBoardUseCase(boardRepository);
+  const searchBoardController = new SearchBoardController(searchBoardUseCase);
+  return await searchBoardController.execute({ request, response });
+};
