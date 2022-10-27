bf1e12c26a765e45db70ab8bedf64a80dee93e55
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-27 12:17:03 +0200

    feat: change type repo

--- src/application/repositories/board/board-repository.ts
+++ src/application/repositories/board/board-repository.ts
@@ -4,7 +4,7 @@ export interface IBoardRepository {
   save: (data: boardStoreType) => Promise<{ id: string }>;
   getId: ({ id }: { id: string }) => Promise<boardStoreType | null>;
   getUserId: ({ userId }: { userId: string }) => Promise<boardStoreType[] | null>;
-  getName: ({ name }: { name: string }) => Promise<{ id: string } | null>;
+  getName: ({ name }: { name: string }) => Promise<boardStoreType[] | null>;
   delete: ({ id }: { id: string }) => Promise<void>;
   getAll: () => Promise<boardStoreType[]>;
 }
