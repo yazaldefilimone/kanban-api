daa91e0650b9fbc278df2394d7f4e0d4f46ae4b5
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-28 23:21:17 +0200

    feat: add update and getName subtask

--- src/application/repositories/subtask-repository.ts
+++ src/application/repositories/subtask-repository.ts
@@ -2,7 +2,9 @@ import { subTaskStoreType } from '~/domain/task/dtos';
 
 export interface ISubTaskRepository {
   save: (data: Omit<subTaskStoreType, 'id'>) => Promise<{ id: string }>;
+  update: (data: subTaskStoreType) => Promise<subTaskStoreType>;
   getId: ({ id }: { id: string }) => Promise<subTaskStoreType | null>;
+  getName: ({ name }: { name: string }) => Promise<{ id: string } | null>;
   getTaskId: ({ taskId }: { taskId: string }) => Promise<subTaskStoreType[] | null>;
   delete: ({ id }: { id: string }) => Promise<void>;
 }
