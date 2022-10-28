96615e97c38a2ddf202e7423c1f96b0d590ff662
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-28 23:31:56 +0200

    feat: create controller of `ActionSubTask` controller

---
+++ src/infrastructure/controllers/subtask/delete-subtask-controller.ts
@@ -0,0 +1,22 @@
+import { IDeleteSubTaskUseCase } from '~/domain/task/use-cases';
+import { IController } from '~/infrastructure/controllers/protocols';
+
+export class DeleteTaskController implements IController {
+  private readonly deleteSubTaskUseCase: IDeleteSubTaskUseCase;
+  constructor(deleteSubTaskUseCase: IDeleteSubTaskUseCase) {
+    this.deleteSubTaskUseCase = deleteSubTaskUseCase;
+  }
+  async execute({ request, response }: IController.Input): Promise<IController.Output> {
+    const payload = request.query;
+    if (JSON.stringify(payload) === '{}' || !payload) {
+      return response.status(204).json({ message: 'query is empty' });
+    }
+
+    const taskOrError = await this.deleteSubTaskUseCase.perform(payload as any);
+    if (taskOrError.isLeft()) {
+      return response.status(400).json({ message: taskOrError.value.message });
+    }
+
+    return response.status(200).json({ message: 'task delete successfully' });
+  }
+}
