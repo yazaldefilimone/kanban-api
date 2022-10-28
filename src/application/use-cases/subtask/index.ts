76324e58f95d4a34307b85c090496a5523c65827
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-28 23:27:04 +0200

    feat: create `SearchSubtask` usecase

---
+++ src/application/use-cases/subtask/index.ts
@@ -0,0 +1,3 @@
+export * from './action-subtask-usecase';
+export * from './delete-subtask-usecase';
+export * from './search-subtask-usecase';
---
+++ src/application/use-cases/subtask/search-subtask-usecase.ts
@@ -0,0 +1,23 @@
+import { ISubTaskRepository } from '~/application/repositories';
+import { InvalidParamError } from '~/domain/errors';
+import { ISearchSubTaskUseCase } from '~/domain/task/use-cases';
+import { left, right } from '~/shared/either';
+
+export class SearchTaskUseCase implements ISearchSubTaskUseCase {
+  private readonly subTaskRepository: ISubTaskRepository;
+  constructor(subTaskRepository: ISubTaskRepository) {
+    this.subTaskRepository = subTaskRepository;
+  }
+
+  async perform(input: ISearchSubTaskUseCase.Input): ISearchSubTaskUseCase.Output {
+    const { taskId } = input;
+
+    if (!taskId) {
+      return left(new InvalidParamError({ param: 'taskId' }));
+    }
+
+    const tasks = await this.subTaskRepository.getTaskId({ taskId });
+
+    return right(tasks);
+  }
+}
