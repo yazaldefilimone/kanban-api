4953802e176181c56a24ea8b9394f560184f4850
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-29 22:03:48 +0200

    feat: register subtask routes

--- src/main/settings/app.ts
+++ src/main/settings/app.ts
@@ -5,6 +5,7 @@ import userRoutes from '~/main/routes/user';
 import taskRoutes from '~/main/routes/task';
 import boardRoutes from '~/main/routes/board';
 import { authMiddlewareFactory } from '~/main/factories/middlewares';
+import subtaskRoutes from '../routes/subtask';
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRoutes);
 app.use('/task', authMiddlewareFactory, taskRoutes);
 app.use('/board', authMiddlewareFactory, boardRoutes);
+app.use('/subtask', authMiddlewareFactory, subtaskRoutes);
 
 export { app };
